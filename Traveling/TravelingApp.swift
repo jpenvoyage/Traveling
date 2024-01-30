e42a5549593611149410b692674e60b16273692b
joaobug <joaoenfrance@gmail.com>
2024-01-30 13:27:49 -0300

    Initial Commit

---
+++ Traveling/TravelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelingApp.swift
+//  Traveling
+//
+//  Created by Joao Leal on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
